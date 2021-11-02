f37242bd4d7f565915ca0aa03f0b4168c1d36042
rani1029 <ranig1029@gmail.com>
2021-11-03 00:51:29 +0530

    startdate and name validation

--- JS/EventListener.js
+++ JS/EventListener.js
@@ -6,7 +6,36 @@ function salaryOutput() {
         output.textContent = salary.value;
     });
 }
+// function validate name
+function validateName() {
+    const name = document.querySelector('#name');
+    const textError = document.querySelector('.text-error');
+    name.addEventListener('input', function () {
+        if (name.value.length == 0) {
+            textError.textContent = "";
+            return;
+        }
+        try {
+            (new EmployeePayrollData()).name = name.value;
+            textError.textContent = "";
+        } catch (e) {
+            console.error(e);
+            textError.textContent = e;
+        }
+    });
+}
+// function to validate date
+function validateDate() {
+    const day = document.querySelector('#day');
+    const month = document.querySelector('#month');
+    const year = document.querySelector('#year');
+    day.addEventListener('input', checkDate);
+    month.addEventListener('input', checkDate);
+    year.addEventListener('input', checkDate);
+}
 
 window.addEventListener('DOMContentLoaded', () => {
     salaryOutput();
+    validateName();
+    validateDate();
 })
